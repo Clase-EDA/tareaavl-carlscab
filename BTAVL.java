e04512c09239f0bf1265813d385a76d54c556015
carlscab <47729568+carlscab@users.noreply.github.com>
2019-10-15 23:56:05 -0500

    Add files via upload

--- BTAVL.java
+++ BTAVL.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  *
  * @author carla
  */
-public class BTAVL<T extends Comparable<T>>{ //extends LinkedBT<T> implements BSTADT<T> {
+public class BTAVL<T extends Comparable<T>> {
     protected int cont;
     protected NodoAVL<T> raiz;
     
@@ -59,8 +59,9 @@ public class BTAVL<T extends Comparable<T>>{ //extends LinkedBT<T> implements BS
                 }
             }
         }
-        if (termine) 
+        if (termine) {
             rotacion(actual);
+        }
         
     }
 
@@ -94,93 +95,149 @@ public class BTAVL<T extends Comparable<T>>{ //extends LinkedBT<T> implements BS
             cont--;
             
             if(temp==raiz){
+                int h=raiz.getFe();
                 //hay hd
                 if(temp.getDer()!=null){
-                    NodoAVL<T> aux=temp.getDer();
-                    NodoAVL<T> hi=raiz.getIzq();
-                    NodoAVL<T> hd=raiz.getDer();
+                    NodoAVL<T> ant=temp.getDer();
+                    NodoAVL<T> act=ant.getIzq();
                     
-                    while(aux.getIzq()!=null){
-                        aux=aux.getIzq();
+                    while(act!=null){
+                        ant=act;
+                        act=ant.getIzq();
                     }
+                    T dato=ant.getElem();
+                    raiz.setElem(dato);
+                    
                     //si no tiene hijos
-                    if(aux.getDer()==null && aux.getIzq()==null){
-                        aux.getPapa().setIzq(null);
-                        rotacion(aux.getPapa());
-                    }//hijo der
-                    if(aux.getDer()!=null){
-                        //si el aux hd es menor al papá del aux
-                        if(aux.getDer().getElem().compareTo(aux.getPapa().getElem())<=0){
-                            aux.getPapa().setIzq(aux.getDer());
-                            rotacion(aux.getPapa());
+                    if(ant.getDer()==null && ant.getIzq()==null){
+                        ant.getPapa().setDer(null);//der
+                        ant.getPapa().setFe(h--);
+                        rotacion(ant.getPapa());                     
+                        ant.setPapa(null);
+
+                    }else{
+                        //hijo der, pero no izq
+                        if(ant.getDer()!=null && ant.getIzq()==null){
+                                ant.getPapa().cuelga(ant.getDer(), ant.getPapa());//der
+                                ant.setPapa(null);
+                                ant.setDer(null);//der
+                                ant.getPapa().setFe();
+                                rotacion(ant.getPapa());
                         }else{
-                            //si el aux hd es mayor al papá del aux
-                            NodoAVL<T> paps=aux.getPapa();
-                            NodoAVL<T> abue=paps.getPapa();
-                            abue.cuelga(aux.getDer(), abue);
-                            aux.getDer().cuelga(paps, aux.getDer());
-                            rotacion(abue);
+                            //hijo izq pero no hijo der
+                            if(ant.getDer()==null && ant.getIzq()!=null){
+                                ant.getPapa().cuelga(ant.getIzq(), ant.getPapa());
+                                ant.setPapa(null);
+                                ant.setIzq(null);//izq
+                                ant.getPapa().setFe();
+                                rotacion(ant.getPapa());
+                            }
+                            else{
+                                //tiene los dos hijos
+                                ant.getPapa().cuelga(ant.getDer(), ant.getPapa());
+                                ant.getDer().cuelga(ant.getIzq(), ant.getDer());
+                                ant.getPapa().setFe();
+                                rotacion(ant.getPapa());
+                                ant.getDer().setFe();
+                                rotacion(ant.getDer());
+                            }
                         }
+                    
                     }
-                    raiz=aux;
-                    raiz.cuelga(hi, raiz);
-                    raiz.cuelga(hd, raiz);
+                    raiz.setFe();
                     temp.setIzq(null);
                     temp.setDer(null);
+                    temp.setPapa(null);
                     return;
                 }
                 //no hay hd
                 else{
+                    //perpo sí hay hi
                     if(temp.getIzq()!=null){
                         raiz=temp.getIzq();
+                        raiz.setFe(h++);
                         rotacion(raiz);
                     }
-                    else
+                    //tampoco hay hi
+                    else{
+                        raiz.setFe(0);
                         raiz=null;
+                    }
                     temp.setIzq(null);
+                    temp.setPapa(null);
+                    //raiz.setFe(0);
                     return;
                 }
             }
             else{
                 //el que buscamos no es la raiz
                 pap=temp.getPapa();
+                int g=pap.getFe();
                 
                 //si no tiene hijos
                 if(temp.getDer()==null && temp.getIzq()==null){
-                    if(pap.getIzq()==temp)
-                        pap.setIzq(null);
-                    else
-                        pap.setDer(null);
-                    temp.setPapa(null);
-                    rotacion(pap);
+                    
+                        if(pap.getIzq()==temp){
+                            pap.setIzq(null);
+                            g++;
+                        }
+                        else{
+                            pap.setDer(null);
+                            g--;
+                        pap.setFe(g);
+                        rotacion(pap);
+                    }
+                    
                 }
                 //hi sí hd nel
                 if(temp.getIzq()!=null && temp.getDer()==null){
-                    pap.setIzq(temp.getIzq());
-                    temp.setPapa(null);
-                    temp.setIzq(null);
-                    rotacion(pap);
+
+                        pap.cuelga(temp.getIzq(), pap);
+                        temp.getIzq().setFe();
+                        rotacion(temp.getIzq());
+                        temp.setIzq(null);
+                        pap.setFe(g--);
+                        rotacion(pap);
+
                 }
                 //hi no hd sí
                 if(temp.getIzq()==null && temp.getDer()!=null){
-                    //hd mayor a papa temp
-                    if(temp.getDer().getElem().compareTo(pap.getElem())<=0){
-                        pap.getPapa().cuelga(temp.getDer(), pap.getPapa());
-                        temp.getDer().cuelga(pap, temp.getDer());
-                        rotacion(temp.getDer());
-                        temp.setPapa(null);
-                        temp.setDer(null);
-                        return;
-                    }else{
-                        //hd menor a pap temp
-                        pap.cuelga(temp.getDer(), pap);
-                        temp.setPapa(null);
-                        temp.setDer(null);
-                        rotacion(pap);
-                        return;
-                    }
+
+                    pap.cuelga(temp.getDer(), pap);
+                    temp.getDer().setFe();
+                    rotacion(temp.getDer());
+                    pap.setFe(g++);
+                    rotacion(pap);
+                    temp.setDer(null);
+                    
                 }
                 
+                temp.setPapa(null);
+                boolean termine;
+                NodoAVL<T> actual = temp;
+                termine = false;
+                while (actual != raiz && !termine) {
+                    if (actual.getFe() == 0 && actual.getDer() != null && actual.getIzq() != null) {
+                        actual = pap;
+                        pap = pap.getPapa();
+                        if (Math.abs(actual.getFe()) == 2) {
+                            termine = true;
+                        }
+                    } else {
+                        if (pap.getDer() == actual) {
+                            pap.setFe(pap.getFe() + 1);
+                        } else {
+                            pap.setFe(pap.getFe() - 1);
+                        }
+                        actual = pap;
+                        pap = pap.getPapa();
+                        if (Math.abs(actual.getFe()) == 2) {
+                            termine = true;
+                        }
+                    }
+                }
+                if (termine) 
+                    rotacion(actual);                
             }
         }
         else
@@ -188,10 +245,13 @@ public class BTAVL<T extends Comparable<T>>{ //extends LinkedBT<T> implements BS
         
     }
     
+    
+
+   
     private NodoAVL<T> rotacion(NodoAVL<T> n){
         //izq-izq
         if(n.getFe()==-2 && n.getIzq().getFe()<=0){
-            
+            System.out.println("izqizq");
             NodoAVL<T> alfa=n;
             NodoAVL<T> papa=n.getPapa();
             NodoAVL<T> beta=alfa.getIzq();
@@ -201,10 +261,17 @@ public class BTAVL<T extends Comparable<T>>{ //extends LinkedBT<T> implements BS
             NodoAVL<T> C=beta.getDer();
             NodoAVL<T> D=alfa.getDer();
             
-            gamma.cuelga(A, gamma);
-            gamma.cuelga(B, gamma);
-            alfa.cuelga(C, alfa);
-            alfa.cuelga(D, alfa);
+            
+            if(A!=null)
+                gamma.cuelga(A, gamma);
+            if(B!=null)
+                gamma.cuelga(B, gamma);
+            if(C!=null)
+                alfa.cuelga(C, alfa);
+            else
+                alfa.setIzq(C);
+            if(D!=null)
+                alfa.cuelga(D, alfa);
             beta.cuelga(alfa, beta);
             beta.cuelga(gamma, beta);
             if(papa!=null)
@@ -220,6 +287,7 @@ public class BTAVL<T extends Comparable<T>>{ //extends LinkedBT<T> implements BS
         }
         //izq-der
         if(n.getFe()==-2 && n.getIzq().getFe()>0){
+            System.out.println("izqder");
             NodoAVL<T> alfa=n;
             NodoAVL<T> papa=n.getPapa();
             NodoAVL<T> beta=alfa.getIzq();
@@ -229,10 +297,18 @@ public class BTAVL<T extends Comparable<T>>{ //extends LinkedBT<T> implements BS
             NodoAVL<T> B=gamma.getIzq();
             NodoAVL<T> C=gamma.getDer();
             
-            beta.cuelga(A, beta);
-            beta.cuelga(B, beta);
-            alfa.cuelga(C, alfa);
-            alfa.cuelga(D, alfa);
+            if(A!=null)
+                beta.cuelga(A, beta);
+            if(B!=null)
+                beta.cuelga(B, beta);
+            else
+                beta.setDer(null);
+            if(C!=null)
+                alfa.cuelga(C, alfa);
+            else
+                alfa.setIzq(null);
+            if(D!=null)
+                alfa.cuelga(D, alfa);
             gamma.cuelga(beta, gamma);
             gamma.cuelga(alfa, gamma);
             
@@ -250,6 +326,7 @@ public class BTAVL<T extends Comparable<T>>{ //extends LinkedBT<T> implements BS
         
         //der-der
         if(n.getFe()==2 && n.getDer().getFe()>=0){
+            System.out.println("derder");
             NodoAVL<T> alfa=n;
             NodoAVL<T> papa=n.getPapa();
             NodoAVL<T> beta=alfa.getDer();
@@ -266,7 +343,7 @@ public class BTAVL<T extends Comparable<T>>{ //extends LinkedBT<T> implements BS
                 alfa.cuelga(B, alfa);
             }
             else{
-                beta.setPapa(null);
+               alfa.setDer(null);
             }
             if(C!=null){
                 gamma.cuelga(C, gamma);
@@ -277,19 +354,23 @@ public class BTAVL<T extends Comparable<T>>{ //extends LinkedBT<T> implements BS
             beta.cuelga(alfa, beta);
             beta.cuelga(gamma, beta);
             
-            if(papa!=null)
+            if(papa!=null){
                 papa.cuelga(beta, papa);
-            else{
+                papa.setFe();
+            }else{
                 beta.setPapa(null);
                 raiz=beta;
+                raiz.setFe();
             }
             alfa.setFe();
             gamma.setFe();
             beta.setFe();
+            
             return beta;   
         }
          //der-izq
         if(n.getFe()==2 && n.getDer().getFe()<0){
+            System.out.println("derizq");
             NodoAVL<T> alfa=n;
             NodoAVL<T> papa=n.getPapa();
             NodoAVL<T> beta=alfa.getDer();
@@ -299,10 +380,18 @@ public class BTAVL<T extends Comparable<T>>{ //extends LinkedBT<T> implements BS
             NodoAVL<T> C=gamma.getDer();
             NodoAVL<T> D=beta.getDer();
             
-            alfa.cuelga(A, alfa);
-            alfa.cuelga(B, alfa);
-            beta.cuelga(C, beta);
-            beta.cuelga(D, beta);
+            if(A!=null)
+                alfa.cuelga(A, alfa);
+            if(B!=null)
+                alfa.cuelga(B, alfa);
+            else
+                alfa.setDer(null);
+            if(C!=null)
+                beta.cuelga(C, beta);
+            else
+                beta.setIzq(null);
+            if(D!=null)
+                beta.cuelga(D, beta);
             gamma.cuelga(alfa, gamma);
             gamma.cuelga(beta, gamma);
             
@@ -354,9 +443,15 @@ public class BTAVL<T extends Comparable<T>>{ //extends LinkedBT<T> implements BS
         NodoAVL<T> temp;
         while (!aux.isEmpty()) {
             temp = aux.remove(0);
-            lista.add(temp.getElem());//problema
-            lista2.add(temp.getFe());
+            
+            //System.out.println(temp.getElem());
+            
+            if(temp.getElem()!=null){
+                lista2.add(temp.getFe());   //problema
+                lista.add(temp.getElem());//problema
+            }
             if (temp.getIzq() != null) {
+                //quitar al agregar
                 aux.add(temp.getIzq());
             }
             if (temp.getDer() != null) {
@@ -370,67 +465,25 @@ public class BTAVL<T extends Comparable<T>>{ //extends LinkedBT<T> implements BS
         }
     }
         
-    /*    //    // mÃ©todo para acomodar los elementos - LEVELORDER
-    public Iterator<T> levelOrder(){
+    public void levelOrder(){
         ArrayList<T> lista= new ArrayList<T>();
-        ColaA <NodoBT<T>> cola= new ColaA();
-        cola.agrega(root);
+        ColaA <NodoAVL<T>> cola= new ColaA();
+        cola.agrega(raiz);
+        ArrayList<Integer> lista2= new ArrayList<>();
+        
         while(!cola.estaVacia()){
-           NodoBT<T> actual=cola.quita();
-            lista.add(actual.getElement());
+           NodoAVL<T> actual=cola.quita();
+            lista.add(actual.getElem());   
+            lista2.add(actual.getFe());     
             if(actual.getDer()!=null)
                 cola.agrega(actual.getDer());
             if(actual.getIzq()!=null)
                 cola.agrega(actual.getIzq()); 
         }
-        return lista.iterator();
-    }//end levelOrder
-*/
-
+        while(lista.iterator().hasNext()){
+            T x = lista.remove(0);
+            int y = lista2.remove(0);
+            System.out.println("Elemento almacenado: " + x + "  Factor de equilibrio: " + y + "\n");
+        }
+    }
 }
-
-    //a partir del nodo insertado
-//    public boolean insertar(T dato){
-//        NodoAVL<T>  act=new NodoAVL(dato);    //el que insertamos
-//        NodoAVL<T>  papa=act.getPapa();
-//        boolean band = false;
-//        
-//        if (raiz == null) {
-//            raiz = act;
-//            cont++;
-//            return true;
-//        }
-//        
-//        
-//        while(!band && papa!=null){
-//            if(act==act.getPapa().getIzq())
-//                act.getPapa().fe-=1;
-//            else
-//                act.getPapa().fe+=1;
-//        }
-//        if(abs(act.getPapa().fe)==2){
-//            act=rotacion(act.getPapa());
-//            band=true;
-//        }
-//        if(act.getPapa().fe==0)
-//            band=true;
-//        act=act.getPapa();
-//        return band;
-//    }
-//    
-//  
-
-//    public Iterator<T> imprimeLevelOrden(){
-//        ArrayList<T> lista= new ArrayList<T>();
-//        ColaA<NodoAVL<T>> cola= new ColaA();
-//        cola.agrega(raiz);
-//        while(!cola.estaVacia()){
-//           NodoAVL<T> actual=cola.quita();
-//            lista.add(actual.getElem());
-//            if(actual.getDer()!=null)
-//                cola.agrega(actual.getDer());
-//            if(actual.getIzq()!=null)
-//                cola.agrega(actual.getIzq()); 
-//        }
-//        return lista.iterator();
-//    }
